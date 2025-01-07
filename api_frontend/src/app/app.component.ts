2cd42dc2727e209ba7a12c9409337854dcfbd17e
CP590 <camran@sky.com>
2025-01-07 11:24:36 +0000

    Pre-Angular 19

---
+++ api_frontend/src/app/app.component.ts
@@ -0,0 +1,32 @@
+import {Component, OnInit} from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {MatSidenav, MatSidenavContainer, MatSidenavContent} from '@angular/material/sidenav';
+import {SidebarComponent} from './sidebar/sidebar.component';
+import {WorkspaceComponent} from './workspace/workspace.component';
+import {HeaderComponent} from './header/header.component';
+import {ApiService} from './api.service';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, MatSidenavContainer, SidebarComponent, MatSidenav, MatSidenavContent, WorkspaceComponent, HeaderComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit {
+  title = 'CAD-FEA-API';
+  message = '';
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit() {
+    this.apiService.getRoot().subscribe(
+        (response) => {
+          this.message = response;
+          console.log(this.message);
+        },
+        (error) => {
+          console.error('Error fetching message: ', error);
+        }
+    );
+  }
+}
